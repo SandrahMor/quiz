f609007739730b4df2c36dd82a647fcd1dcd8763
sandrah <morandesandy@gmail.com>
2020-03-10 23:47:10 -0500

    update

---
+++ script.js
@@ -0,0 +1,36 @@
+//Begin by setting up timer
+var seconds = document.querySelector("#seconds")
+
+var seconds = 60
+
+
+//create a function to stop the timer when time is up
+function renderTime() {
+    totalSeconds = document.getElementById('seconds').innerHTML;
+    if (totalSeconds >= 60) {
+        totalSeconds--
+        console.log(totalSeconds)
+    }
+      
+}
+renderTime()
+
+    
+
+
+// create an object of the questions to be displayed 
+var questions {
+
+
+
+
+}
+
+//var to log the scores
+
+
+// randomize the selection of questions and render them onto the screen when quiz is initialized
+
+
+
+
